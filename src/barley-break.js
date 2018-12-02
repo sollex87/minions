33316dd9583dd9ca6bf4795efc2e3ebe4b413c93
sollex87 <sollex87@gmail.com>
2018-12-02 15:04:49 +0200

    added click sound on successful click in Barley Break

--- src/barley-break.js
+++ src/barley-break.js
@@ -1,109 +1,123 @@
 export default function BarleyBreak(context, cellSize) {
-  const arr = [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [9, 10, 11, 12],
-      [13, 14, 15, 0]
-  ];
-  let clicks = 0;
+    const arr = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 0]
+    ];
 
-  function cellView(x, y) {
-      context.clearRect(x + 1, y + 1, cellSize - 2, cellSize - 2); // очистка ячейки
-  }
+    let clicks = 0;
 
-  function numView() {
-    context.font = "normal " + (cellSize / 3) + "px Gloria Hallelujah, sans-serif"; // стилизация чисел
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    // context.shadowOffsetX = "-2";
-    // context.shadowOffsetY = "-2";
-    // context.shadowColor = "#f8c790";
-    // context.shadowBlur = 1;
-    context.fillStyle = "#000000";
-  }
+    function playClick() {
+        console.log('click');
+        const clickSound = new Audio();
+        clickSound.src = require('../assets/click.mp3');
+        clickSound.autoplay = true;
+    }
 
-  this.getNullCell = function () { // определение пустой ячейки
-      for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-              if (arr[j][i] === 0) {
-                  return {'x': i, 'y': j};
-              }
-          }
-      }
-  };
+    function cellView(x, y) {
+        context.clearRect(x + 1, y + 1, cellSize - 2, cellSize - 2); // очистка ячейки
+    }
 
-  this.draw = function () { // основная отрисовка
-      for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-              if (arr[i][j] > 0) {
-                  cellView(j * cellSize, i * cellSize);
-                  numView();
-                  context.fillText(arr[i][j], j * cellSize + cellSize / 2, i * cellSize + cellSize / 2);
-              }
-          }
-      }
-  };
+    function numView() {
+        context.font = "normal " + (cellSize / 3) + "px Gloria Hallelujah, sans-serif"; // стилизация чисел
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillStyle = "#000000";
+    }
 
-  this.move = function (x, y) { // движение
-      let nullX = this.getNullCell().x;
-      let nullY = this.getNullCell().y;
-      if (((x - 1 == nullX || x + 1 == nullX) && y == nullY) || ((y - 1 == nullY || y + 1 == nullY) && x == nullX)) {
-          arr[nullY][nullX] = arr[y][x];
-          arr[y][x] = 0;
-          clicks++;
-      }
-  };
+    this.getNullCell = function () { // определение пустой ячейки
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (arr[j][i] === 0) {
+                    return { 'x': i, 'y': j };
+                }
+            }
+        }
+    };
 
-  this.victory = () => { // в случае победы
-      const e = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
-      let res = true;
-      for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-              if (e[i][j] != arr[i][j]) {
-                  res = false;
-              }
-          }
-      }
-      return res;
-  };
+    this.draw = function () { // основная отрисовка
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (arr[i][j] > 0) {
+                    cellView(j * cellSize, i * cellSize);
+                    numView();
+                    context.fillText(arr[i][j], j * cellSize + cellSize / 2, i * cellSize + cellSize / 2);
+                }
+            }
+        }
+    };
 
-  function getRandom() {
-      if (Math.floor(Math.random() * 2) === 0) {
-          return true;
-      }
-  }
+    this.shuffle = function (x, y) { // движение
+        let nullX = this.getNullCell().x;
+        let nullY = this.getNullCell().y;
+        if (((x - 1 == nullX || x + 1 == nullX) && y == nullY) || ((y - 1 == nullY || y + 1 == nullY) && x == nullX)) {
+            arr[nullY][nullX] = arr[y][x];
+            arr[y][x] = 0;
+        }
+    };
 
-  this.mix = function (stepCount) { //перемешивание
-      let x, y;
-      for (let i = 0; i < stepCount; i++) {
-          const nullX = this.getNullCell().x;
-          const nullY = this.getNullCell().y;
-          const hMove = getRandom();
-          const upLeft = getRandom();
-          if (!hMove && !upLeft) {
-              y = nullY;
-              x = nullX - 1;
-          }
-          if (hMove && !upLeft) {
-              x = nullX;
-              y = nullY + 1;
-          }
-          if (!hMove && upLeft) {
-              y = nullY;
-              x = nullX + 1;
-          }
-          if (hMove && upLeft) {
-              x = nullX;
-              y = nullY - 1;
-          }
-          if (0 <= x && x <= 3 && 0 <= y && y <= 3) {
-              this.move(x, y);
-          }
-      }
-      clicks = 0;
-  };
+    this.move = function (x, y) { // движение
+        let nullX = this.getNullCell().x;
+        let nullY = this.getNullCell().y;
+        if (((x - 1 == nullX || x + 1 == nullX) && y == nullY) || ((y - 1 == nullY || y + 1 == nullY) && x == nullX)) {
+            arr[nullY][nullX] = arr[y][x];
+            arr[y][x] = 0;
+            clicks++;
+            playClick();
+        }
+    };
 
-  this.getClicks = () => {
-      return clicks;
-  };
+    this.victory = () => { // в случае победы
+        const e = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
+        let res = true;
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (e[i][j] != arr[i][j]) {
+                    res = false;
+                }
+            }
+        }
+        return res;
+    };
+
+    function getRandom() {
+        if (Math.floor(Math.random() * 2) === 0) {
+            return true;
+        }
+    }
+
+    this.mix = function (stepCount) { //перемешивание
+        let x, y;
+        for (let i = 0; i < stepCount; i++) {
+            const nullX = this.getNullCell().x;
+            const nullY = this.getNullCell().y;
+            const hMove = getRandom();
+            const upLeft = getRandom();
+            if (!hMove && !upLeft) {
+                y = nullY;
+                x = nullX - 1;
+            }
+            if (hMove && !upLeft) {
+                x = nullX;
+                y = nullY + 1;
+            }
+            if (!hMove && upLeft) {
+                y = nullY;
+                x = nullX + 1;
+            }
+            if (hMove && upLeft) {
+                x = nullX;
+                y = nullY - 1;
+            }
+            if (0 <= x && x <= 3 && 0 <= y && y <= 3) {
+                this.shuffle(x, y);
+            }
+        }
+        clicks = 0;
+    };
+
+    this.getClicks = () => {
+        return clicks;
+    };
 }
