0cd2b17dab2beb55c4c88fb59a8fbb08c2074805
sollex87 <sollex87@gmail.com>
2018-12-05 14:16:42 +0200

    added images pathing

--- src/memory.js
+++ src/memory.js
@@ -1,8 +1,22 @@
 export default class Memory {
   constructor() {
-    this.suits = ['0001', '0002', '0003', '0004', '0005', '0006',
-      '0007', '0008', '0001', '0002', '0003', '0004', '0005', '0006',
-      '0007', '0008',]
+    this.suits = [
+      '0000',
+      '0001',
+      '0002',
+      '0003',
+      '0004',
+      '0005',
+      '0006',
+      '0007',
+      '0000',
+      '0001',
+      '0002',
+      '0003',
+      '0004',
+      '0005',
+      '0006',
+      '0007',]
     this.newSuits = this.mixSuits(this.suits);
     this.memory = document.getElementById('memory');
     this.clicksDisplay = document.getElementById("span-clicks");
@@ -12,7 +26,16 @@ export default class Memory {
     this.lockBoard = false;
     this.countTries = 0;
     this.countPairs = 0;
-    this.backURLs = {};
+    this.backURLs = {
+      '0000': require('../assets/memory/0000.jpg'),
+      '0001': require('../assets/memory/0001.jpg'),
+      '0002': require('../assets/memory/0002.jpg'),
+      '0003': require('../assets/memory/0003.jpg'),
+      '0004': require('../assets/memory/0004.jpg'),
+      '0005': require('../assets/memory/0005.jpg'),
+      '0006': require('../assets/memory/0006.jpg'),
+      '0007': require('../assets/memory/0007.jpg'),
+    };
     this.frontURL = require('../assets/memory/back.jpg');
     const _that = this;
     this.flipCard = function () {
@@ -30,25 +53,12 @@ export default class Memory {
     }
   }
 
-  getUrls() {
-    for (let i = 0; i < this.suits.length / 2; i += 1) {
-      if (i < 10) {
-        this.backURLs[i] = `../assets/memory/000${i + 1}.jpg`;
-      }
-      console.log(this.backURLs);
-    }
-  }
-
   mixSuits(suits) {
     return suits.sort(() => { return .5 - Math.random() });
   }
 
   createCards() {
-    this.getUrls();
-    console.log(this.newSuits);
-    console.log(this.frontURL);
     this.newSuits.map((newSuits, i) => {
-      console.log(this.frontURL);
       let card = document.createElement('div');
       let img_front = document.createElement('img');
       let img_back = document.createElement('img');
@@ -58,24 +68,11 @@ export default class Memory {
       card.name = `${newSuits}`;
       memory.prepend(card);
       img_back.className = 'back';
-      img_back.src = `https://raw.githubusercontent.com/Boardonly/images/master/images/${newSuits}.jpg`;
+      img_back.src = this.backURLs[newSuits];
       img_front.className = 'front';
       img_front.src = this.frontURL;
       card.prepend(img_back, img_front);
     })
-  }
-
-  flipCard() {
-    if (this.lockBoard) return;
-    if (this === this.firstCard) return;
-    this.classList.add('flip');
-    if (this.hasFlippedCard == false) {
-      this.hasFlippedCard = true;
-      this.firstCard = this;
-      return;
-    }
-    this.secondCard = this;
-    () => matchCard();
   }
 
   unflipCard() {
